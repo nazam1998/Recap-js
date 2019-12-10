3ad330cca30d8b568b980334618cd5e6ac5fc0d0
Nazam <nazam98-be@email.com>
2019-12-10 20:30:26 +0100

    exo2 done

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,34 @@
 let link = document.querySelectorAll('#nav a');
-link.forEach(e => {
-    e.addEventListener('click', () => {
-        link.forEach(a => {
-            a.classList.replace('text-white', 'text-primary');
-            a.classList.replace('bg-primary', 'bg-white');
+let classe = document.getElementsByTagName('section');
+let panel = []
+for (let i = 0; i < link.length; i++) {
+    panel.push({
+        lien: link[i],
+        exo: classe[i]
+    });
+    if (i != 0) {
+        classe[i].style.display = 'none';
+    }
+}
+let currentPan = panel[0].exo.getAttribute('class');
+panel.forEach(e => {
+    e.lien.addEventListener('click', () => {
+        panel.forEach(a => {
+            a.lien.classList.replace('text-white', 'text-primary');
+            a.lien.classList.replace('bg-primary', 'bg-white');
+            a.exo.style.display = 'none';
         });
-        e.classList.replace('text-primary', 'text-white');
-        e.classList.replace('bg-white', 'bg-primary');
-    })
+
+
+        e.lien.classList.replace('text-primary', 'text-white');
+        e.lien.classList.replace('bg-white', 'bg-primary');
+        e.exo.style.display = 'block';
+        currentPan = e.exo.getAttribute('class');
+    });
 });
 
 
-
+// Exo 2
 
 let titre = document.querySelector('.exo1 h2');
 let button = document.querySelector('.exo1 button');
@@ -19,5 +36,7 @@ let button = document.querySelector('.exo1 button');
 button.addEventListener('click', () => {
     titre.innerHTML = 'titre modifié';
     titre.classList.add('bg-primary');
-    titre.classList.add('text-white');
+    titre.classList.add('text-danger');
 });
+
+// Exo 3
