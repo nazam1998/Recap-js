26651debfbdfff183877957ac35d5572c64aafee
Nazam <nazam98-be@email.com>
2019-12-10 20:43:46 +0100

    exo3 done

--- public/js/main.js
+++ public/js/main.js
@@ -40,3 +40,17 @@ button.addEventListener('click', () => {
 });
 
 // Exo 3
+
+let exo3 = document.getElementsByClassName('exo2')[0];
+let h2 = document.querySelector('.exo3 h2');
+let input = document.querySelector('.exo3 input');
+let button2 = input.nextElementSibling;
+
+input.addEventListener('keypress', event => {
+    input.value = event.target.value;
+});
+button2.addEventListener('click', () => {
+    h2.innerHTML = 'Welcome ' + input.value;
+    input.value = '';
+});
+
