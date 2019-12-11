ccb5cd16cc91753ede7e41da468a053e0a524ec8
Nazam <nazam98-be@email.com>
2019-12-11 15:39:15 +0100

    exo6 done

--- README.md
+++ README.md
@@ -4,4 +4,3 @@
     - Js
     - Emmet
     - Sass
-    - 
--- public/js/main.js
+++ public/js/main.js
@@ -13,7 +13,6 @@ for (let i = 0; i < link.length; i++) {
 
 panel.forEach(e => {
     e.lien.addEventListener('click', () => {
-        console.log(e);
 
         panel.forEach(a => {
             a.lien.classList.replace('text-white', 'text-primary');
@@ -29,7 +28,7 @@ panel.forEach(e => {
 });
 
 
-// Exo 2
+// Exo 1
 
 let titre = document.querySelector('.exo1 h2');
 let button = document.querySelector('.exo1 button');
@@ -40,7 +39,7 @@ button.addEventListener('click', () => {
     titre.classList.add('text-danger');
 });
 
-// Exo 3
+// Exo 2
 
 let exo3 = document.getElementsByClassName('exo2')[0];
 let h2 = document.querySelector('.exo3 h2');
@@ -55,7 +54,7 @@ button2.addEventListener('click', () => {
     input.value = '';
 });
 
-// Exo 4
+// Exo 3
 
 let rectangle = document.getElementsByClassName('rectangle');
 let btnSwitch = document.querySelectorAll('.exo4 button');
@@ -71,11 +70,10 @@ btnSwitch[0].addEventListener('click', () => {
         rectangle[1].removeChild(square);
         rectangle[0].appendChild(square);
     }
-    console.log(count);
     btnSwitch[1].innerHTML = `nombre de click ${count}`
 });
 
-// Exo 5
+// Exo 4
 
 let generator = document.querySelector('.exo5 button');
 
@@ -86,7 +84,7 @@ generator.addEventListener('click', () => {
     generator.nextElementSibling.appendChild(image);
 });
 
-// Exo 6
+// Exo 5
 
 let calcul = document.getElementsByClassName('level1')[0];
 let nombre = document.querySelectorAll('.level1 input');
@@ -96,3 +94,74 @@ egal.addEventListener('click', () => {
     egal.nextElementSibling.innerHTML = Number(nombre[0].value) + Number(nombre[1].value);
 });
 
+// Exo 6
+
+let egal2 = document.getElementById('egal');
+let clear = document.getElementById('clear');
+let number = document.querySelectorAll('.number');
+let op = document.querySelectorAll('.op');
+
+let input2 = document.querySelector('.level2 input');
+let p = document.querySelector('.level2 p');
+let res;
+let num1 = '';
+let num2 = '';
+let signe;
+let cpt = 0;
+let save;
+
+op.forEach(e => {
+    e.addEventListener('click', () => {
+        input2.value += e.innerHTML;
+        signe = e.innerHTML;
+        cpt++;
+    });
+});
+number.forEach(a => {
+    a.addEventListener('click', () => {
+        if (cpt % 2 == 1) {
+            num2 += a.innerHTML;
+            input2.value += a.innerHTML;
+
+
+        } else {
+            num1 += a.innerHTML;
+            input2.value += a.innerHTML;
+
+        }
+        p.innerHTML = input2.value;
+    });
+});
+
+egal2.addEventListener('click', () => {
+
+    switch (signe) {
+        case '+':
+            res = Number(num1) + Number(num2);
+            break;
+
+        case '-':
+            res = Number(num1) - Number(num2);
+            break;
+        case '*':
+            res = Number(num1) * Number(num2);
+            break;
+        case '/':
+            res = Number(num1) / Number(num2);
+            break;
+    }
+
+    console.log(res);
+    num1 = 0;
+    num2 = 0;
+    p.innerHTML = res;
+});
+
+clear.addEventListener('click', () => {
+    p.innerHTML = 0;
+    input2.value = '';
+    num1 = '';
+    num2 = '';
+});
+
+// Exo 7
